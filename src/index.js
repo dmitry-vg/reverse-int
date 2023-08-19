adfccf5c980a99832d1f8b446ba76c78e9726eac
Dzmitry Yushkevich <ip.yshkevich@gmail.com>
2023-08-19 22:14:28 +0300

    comlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  n = String(n);
+  let arr = n.split('');
+  if (arr[0] == '-'){
+    arr.shift();
+    
+  } 
+    arr.reverse();
+    n = arr.join('');
+        return Number(n);
   
 }
